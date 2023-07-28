d973a62b1404e12cf1a3af4ae2995508866c97b1
xz-dev <xiangzhedev@gmail.com>
2023-07-28 22:39:01 +0800

    fix: image build with tree level

--- src/distrobox_config_converter.rs
+++ src/distrobox_config_converter.rs
@@ -5,32 +5,29 @@ use crate::container_tree::distrobox_assemble_tree::{trees_to_distrobox_assemble
 use crate::distrobox_parser::assemble::ContainerAssembleData;
 use crate::oci::image_builder::build_image;
 
-fn build_image_by_tree(container_runner: &str, tree: &ContainerNode) -> HashMap<String, String> {
-    let mut image_map = HashMap::new();
-    fn tree_to_image_map(
-        container_runner: &str,
-        tree: &ContainerNode,
-        image_map: &mut HashMap<String, String>,
-    ) {
-        let image = &tree.container_assemble_data.image;
-        if !image_map.contains_key(image) {
-            let empty_vec = Vec::new();
-            let packages = tree
-                .container_assemble_data
-                .packages
-                .as_ref()
-                .unwrap_or(&empty_vec);
-            let new_image = format!("distrobox-{}", &image);
-            println!("Build container name: {} to {}", &tree.container_name, &new_image);
-            build_image(container_runner, &new_image, &image, packages).unwrap();
-            image_map.insert(image.clone(), new_image);
-        }
-        for child in &tree.children {
-            tree_to_image_map(container_runner, child, image_map);
+fn build_image_by_tree(container_runner: &str, tree: &mut ContainerNode) {
+    fn tree_to_image_map(container_runner: &str, tree: &mut ContainerNode, node_level: usize) {
+        let image = &tree.container_assemble_data.image.clone();
+        println!("Build image: {}", &image);
+        let empty_vec = vec![];
+        let packages = tree
+            .container_assemble_data
+            .packages
+            .as_ref()
+            .unwrap_or(&empty_vec);
+        println!("Packages: {:?}", &packages);
+        let new_image = format!("distrobox-{}-{}", node_level, &image);
+        println!(
+            "Build container name: {} to {}",
+            &tree.container_name, &new_image
+        );
+        build_image(container_runner, &new_image, &image, packages).unwrap();
+        tree.container_assemble_data.image = new_image.clone();
+        for child in &mut tree.children {
+            tree_to_image_map(container_runner, child, node_level + 1);
         }
     }
-    tree_to_image_map(container_runner, tree, &mut image_map);
-    image_map
+    tree_to_image_map(container_runner, tree, 0);
 }
 
 pub fn build_distrobox_assemble_data(
@@ -39,21 +36,14 @@ pub fn build_distrobox_assemble_data(
 ) -> HashMap<String, ContainerAssembleData> {
     let mut trees = build_container_trees(data);
 
-    fn change_tree_image(tree: &mut ContainerNode, image_map: &HashMap<String, String>) {
-        let image = &tree.container_assemble_data.image;
-        if image_map.contains_key(image) {
-            tree.container_assemble_data.image = image_map[image].clone();
-            tree.container_assemble_data.pull = Some(false);
-        }
-        for child in &mut tree.children {
-            change_tree_image(child, image_map);
-        }
-    }
-
     for tree in &mut trees {
-        let image_map = build_image_by_tree(container_runner, tree);
-        change_tree_image(tree, &image_map);
+        build_image_by_tree(container_runner, tree);
     }
 
-    trees_to_distrobox_assemble(&trees)
+    let mut new_data = trees_to_distrobox_assemble(&trees);
+    for (key, value) in new_data.iter_mut() {
+        value.pull = Some(false);
+        value.packages = data[key].packages.clone();
+    }
+    new_data
 }
