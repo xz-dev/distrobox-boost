e7e64b082bb56fc6df1fbe36ebf5070e58297bd1
xz-dev <xiangzhedev@gmail.com>
2023-08-01 16:25:09 +0800

    ci: mk_cargo_vender.sh for packaging

---
+++ mk_cargo_vender.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+mkdir .cargo
+cargo vendor > .cargo/config
+
+version=$1
+tar czf distrobox-boost-$version.tar.gz --transform "s,^,distrobox-boost-$version/," --exclude mk_cargo_vender.sh --exclude distrobox-boost-*.tar.gz --exclude .git --exclude target * .cargo
+rm -rf vender
+rm -rf .cargo
